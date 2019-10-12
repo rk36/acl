2fda9f54f605ab3cd04e2596223d7c616f12e27b
rk36 <rahulkr31.01@gmail.com>
2019-10-12 13:15:40 +0530

    information about the dates  missed classes

--- source/cli.py
+++ source/cli.py
@@ -1,8 +1,9 @@
 import click
 import keyring
 import getpass
-from source.scrapper import attempt
+from scrapper import attempt
 from tabulate import tabulate
+from missedClassScrapper import MissedClassDates
 
 @click.command()
 @click.option('-r', '--roll', prompt='Roll Number', help='Enter the Roll Number for ERP Login.')
@@ -27,10 +28,18 @@ def attendance(roll):
         saved_password = False
 
     # Fetch attendance from ERP and Pretty Print it on Terminal.
-    table = make_table(response)
-    print(tabulate(table, headers=["Subject Name", "Attended", "Percentage"],
-            tablefmt="fancy_grid"))
+    attendance_table = make_table(response)
+    show_table(attendance_table)
 
+    # Fetch missed classes from ERP and Pretty Print it on Terminal.
+    ans = input("Do you want to see the missed class(es) date-wise ? (y/N) ")
+    if ans=='y':
+        print("please wait, it may take a while to fetch the information... \n")
+        missed_class_response = MissedClassDates(roll,password)
+        missed_class_table = make_missed_class_table(missed_class_response)
+        show_table(missed_class_table)
+
+        
     # Store password locally if not saved already
     if not saved_password:
         ans = input("Do you want to store your password locally? (y/N) ")
@@ -45,6 +54,17 @@ def make_table(response):
         row.append(data['name'])
         row.append(data['attended'] + '/' + data['total'])
         row.append(data['percentage'])
+        result.append(row)
+
+    return result
+
+def make_missed_class_table(response):
+    result = list()
+    for (Date, data) in response.items():
+        row = list()
+        row.append(data['Date'])
+        row.append(data['subjectName'])
+        row.append(data['attended'] + '/' + data['total'])
         result.append(row)
 
     return result
@@ -68,6 +88,11 @@ def ResponseAttempt(roll, password):
         exit(0)
 
     return response,password
+
+
+def show_table(table):
+    print(tabulate(table, headers=["Date","Subject Name", "Attended"],tablefmt="fancy_grid"))
+
 
 
 
---
+++ source/missedClassScrapper.py
@@ -0,0 +1,108 @@
+from robobrowser import RoboBrowser
+
+from bs4 import BeautifulSoup
+import click
+import sys 
+import datetime
+from tabulate import tabulate
+
+
+def MissedClassDates(user, password):
+    
+    url = 'http://erp.iitbbs.ac.in'
+    browser = RoboBrowser(history=False, parser='html.parser')
+    try:
+        browser.open(url)
+    except:
+        click.echo(click.style('Network error, please check your internet connection and firewall for URL: http://erp.iitbbs.ac.in \n', fg='red', bold=True))
+        exit(0) 
+
+    try:
+        form = browser.get_form(action='login.php')
+    except:
+        click.echo(click.style('Network error, please check your internet connection and firewall for URL: http://erp.iitbbs.ac.in \n', fg='red', bold=True))
+        exit(0)
+
+    if not form:
+        click.echo(click.style('Network error, Unable to fetch form \n', fg='red', bold=True))
+        exit(0)
+
+    
+    form['email'].value = user
+    form['password'].value = password
+
+    try:
+        browser.submit_form(form)
+    except:
+        click.echo(click.style('Network error, please check your internet connection and firewall for URL: http://erp.iitbbs.ac.in \n', fg='red', bold=True))
+        exit(0)
+
+    if (browser.url != 'http://erp.iitbbs.ac.in/home.php'):
+        return False
+
+    attendance_link = 'http://erp.iitbbs.ac.in/biometric/list_students_date_wise.php'
+
+    try:
+        browser.open(attendance_link)
+    except:
+        click.echo(click.style('Network error, please check your internet connection and firewall for URL: http://erp.iitbbs.ac.in \n', fg='red', bold=True))
+        exit(0)
+
+    today = datetime.date.today() 
+
+    start_date = "2019-07-19" #start of semester 
+    end_date = today.strftime("%Y-%m-%d")
+    st = RangeOfDate(start_date,end_date)
+
+    return ResponseTable(st,browser)
+
+
+def RangeOfDate(startDate,EndDate):
+    start = datetime.datetime.strptime(startDate, "%Y-%m-%d")
+    end = datetime.datetime.strptime(EndDate, "%Y-%m-%d")
+    date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days+1)]
+    DateRange = []
+    for date in date_generated:
+        DateRange.append(date.strftime("%Y-%m-%d")) 
+    return DateRange
+
+
+def ResponseTable(Dates,browser):
+    result = dict()
+    for Datevalue in Dates:
+        try:
+            Date_form = browser.get_form(action='list_students_date_wise.php')
+        except:
+            click.echo(click.style('Network error, please check your internet connection and firewall for URL: http://erp.iitbbs.ac.in \n', fg='red', bold=True))
+            exit(0)
+
+        Date_form['from'] = Datevalue
+        Date_form['to'] = Datevalue
+
+        try:
+            browser.submit_form(Date_form)
+        except:
+            click.echo(click.style('Network error, please check your internet connection and firewall for URL: http://erp.iitbbs.ac.in \n', fg='red', bold=True))
+            exit(0)
+
+        Date_soup = BeautifulSoup(browser.response.text, 'html.parser')
+        table = Date_soup.find('table', attrs={'border': '1'})
+
+        tr = table.find_all('tr')
+        for row in tr[1:]: # Don't need headers
+            td = row.find_all('td')
+            code = td[0].text.strip()
+            subject = td[1].text.strip()
+            attended_class = td[2].text.strip()
+            total_class = td[3].text.strip()
+            percentage = td[4].text.strip()
+            if(total_class != attended_class):
+                result[Datevalue] = {
+                'Date' :  Datevalue,
+                'subjectName' : subject,
+                'attended': attended_class,
+                'total': total_class,
+                'percentage': percentage
+                }
+
+    return result
